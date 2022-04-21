d170dad95bb7d934b6fd445fa80adcd9f6174b91
Mariya Arshinina <mariya.arshinina@mail.ru>
2022-04-21 06:42:02 +0300

    correction for hexlet test

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -15,7 +15,7 @@ def calc():
     random_number2 = random.randint(1, 100)
     operand = ['+', '-', '*']
     random_operand = random.choice(operand)
-    question = str(random_number1) + random_operand + str(random_number2)
+    question = str(random_number1) + str(random_operand) + str(random_number2)
     result = eval(question)
     result = str(result)
     return question, result
